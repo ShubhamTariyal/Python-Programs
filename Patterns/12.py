43b9054633d198636f175c6cdcabc58270aa028c
Shubham Tariyal <48857023+ShubhamTariyal@users.noreply.github.com>
2020-02-11 22:26:58 +0530

    12th pattern
    used 2 for loop.

---
+++ Patterns/12.py
@@ -0,0 +1,23 @@
+'''
+
+Output:
+-------
+
+Enter value:5
+        1   
+      2   2   
+    3   3   3   
+  4   4   4   4   
+5   5   5   5   5 
+
+
+'''
+
+n = int(input('Enter value:'))
+for i in range (n):
+    for j in range(n):
+        if i >= j - n + 1 and i + j >= n - 1:
+            print(i + 1,end='   ')
+        else:
+            print(end='  ')
+    print()
